62276bb2fa9b83cb58d90a51307d37263b80c3b9
dias <dialaksmana@gmail.com>
2019-07-25 16:49:07 +0700

    chart edit

--- js/chart.js
+++ js/chart.js
@@ -35,3 +35,70 @@ var myChart = new Chart(ctx, {
         }
     }
 });
+
+//myDoughnut
+
+var ctx = document.getElementById('myDoughnut');
+var myDoughnut = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: ['Angular', 'Vue', 'React', 'Laravel'],
+        datasets: [{
+            label: 'Grafik Visited',
+            data: [12, 19, 3, 5],
+            backgroundColor: [
+                '#C30E2E',
+                '#41B883',
+                '#06C7EA',
+                '#F9503B'
+            ],
+            borderColor: [
+                '#C30E2E',
+                '#41B883',
+                '#06C7EA',
+                '#F9503B'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+//myBarChart 
+
+var ctx = document.getElementById('myBarChart ');
+var myBarChart  = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Schmidt Ltd', 'Nienow PLC', 'Breitenberg Group', 'Abbott Ltd'],
+        datasets: [{
+            label: 'Number of users company',
+            data: [70, 50, 50, 45],
+            backgroundColor: [
+                '#CE93D8',
+                '#4884EE',
+                '#5DE6DE',
+                '#fff'
+            ],
+            borderColor: [
+                '#CE93D8',
+                '#4884EE',
+                '#5DE6DE',
+                '#fff'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+        }
+    }
+});
