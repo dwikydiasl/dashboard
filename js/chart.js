b3054bd9f326c963462e82561d7f49331fab3a1b
dias <dialaksmana@gmail.com>
2019-07-25 16:50:32 +0700

    update

--- js/chart.js
+++ js/chart.js
@@ -4,7 +4,7 @@ var myChart = new Chart(ctx, {
     data: {
         labels: ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni'],
         datasets: [{
-            label: 'Grafik Visited',
+            label: 'Grafik Order Project',
             data: [12, 19, 3, 5, 2, 3],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
