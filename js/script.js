e723e8197682dc39c66b0bb68ba23bb1b34458c8
dias <dialaksmana@gmail.com>
2019-08-09 09:23:16 +0700

    update

--- js/script.js
+++ js/script.js
@@ -42,3 +42,9 @@ function search() {
   }
 }
 
+//floating form
+$("input[type='text']").click(function(){
+  var $this=$(this);
+  $this.attr("placeholder","");
+  $this.parent().find("label").addClass("label-top");
+});
